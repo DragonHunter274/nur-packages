01768fc1152ad53facfe34b9dc8c0c8938cd2fee
DragonHunter274 <schurgel+github@gmail.com>
2024-10-22 11:27:37 +0000

    add openbeken-flasher again

--- pkgs/openbeken-flasher/default.nix
+++ pkgs/openbeken-flasher/default.nix
@@ -1,28 +1,36 @@
-{ lib
-, buildDotnetModule
-, fetchFromGitHub
-}:
-
-buildDotnetModule rec {
+{ pkgs, lib, ... }:
+pkgs.stdenv.mkDerivation rec {
   pname = "openbeken-flasher";
   version = "1.3";
 
-  src = fetchFromGitHub {
+  src = pkgs.fetchFromGitHub {
     owner = "openshwprojects";
     repo = "BK7231GUIFlashTool";
     rev = "v${version}";
     sha256 = "sha256-FN2rVKqYzgbIoLlSnLSTegpoSu5U41mAmpw0erzF4dQ=";
   };
 
-  projectFile = "BK7231Flasher.sln";
-  executables = [ "beken-flasher" ];
-  nugetDeps = ./deps.nix;
+  buildInputs = [ pkgs.mono pkgs.makeWrapper ];
+
+  buildPhase = ''
+    xbuild /p:Configuration=Debug BK7231Flasher.sln
+  '';
+
+  installPhase = ''
+    mkdir -p $out/lib
+    cp -r BK7231Flasher/bin/Debug/* $out/lib/
+    
+    mkdir -p $out/bin
+    makeWrapper ${pkgs.mono}/bin/mono $out/bin/openbeken-flasher \
+      --add-flags "$out/lib/BK7231Flasher.exe"
+  '';
 
   meta = with lib; {
-    description = "GUI Flash tool for BK7231 WiFi chips";
+    description = "OpenBK7231 GUI Flash Tool for BK7231/BK7231N chips";
     homepage = "https://github.com/openshwprojects/BK7231GUIFlashTool";
-    license = licenses.gpl3;
-    maintainers = with maintainers; [  ];
-    platforms = platforms.unix;
+    license = lib.licenses.gpl3;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
   };
 }
+
